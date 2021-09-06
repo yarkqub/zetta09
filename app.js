8c524a92336216985676005f2fc543d2a47109ab
Yarkqub <myarkqub@gmail.com>
2021-09-06 21:29:54 +0800

    Add player to room

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const saltRounds = 10
 
 app.use('/', express.static(__dirname + "/client"))
 
+const rooms = []
+
 io.on("connection", socket => {
     let ip = "127.0.0.1"
     const get_ip = socket.request.connection.remoteAddress.split("::ffff:")[1]
@@ -30,7 +32,7 @@ io.on("connection", socket => {
                         bcrypt.hash(data.password, saltRounds, function (err, hash) {
                             const timenow = new Date()
                             const gettime = Math.floor(timenow.getTime() / 1000)
-                            db.prepare("INSERT INTO users (username, mail, password, credits, account_created, ip_last, ip_reg, home_room) VALUES (?, ?, ?, ?, ?, ?, ?, ?)").run(data.username, data.email, hash, 5000, gettime, ip, ip, 1)
+                            db.prepare("INSERT INTO users (username, mail, password, credits, account_created, ip_last, ip_reg, home_room) VALUES (?, ?, ?, ?, ?, ?, ?, ?)").run(data.username, data.email, hash, 5000, gettime, ip, ip, 8)
                             socket.emit("message", { color: "green", msg: "Account created, login to continue" })
                         })
                     }
@@ -50,6 +52,7 @@ io.on("connection", socket => {
             const user = db.prepare("SELECT * FROM users WHERE username = ? COLLATE NOCASE").get(data.username)
             bcrypt.compare(data.password, user.password, function (err, result) {
                 if (result) {
+                    socket.uid = user.id
                     const timenow = new Date()
                     const gettime = Math.floor(timenow.getTime() / 1000)
                     db.prepare("UPDATE users SET last_online = ?, ip_last = ? WHERE id = ?").run(gettime, ip, user.id)
@@ -64,11 +67,47 @@ io.on("connection", socket => {
             socket.emit("message", { color: "red", msg: "Username not found" })
         }
     })
+    socket.on("enter", () => {
+        //get room info
+        const user = db.prepare("SELECT * FROM users WHERE id = ?").get(socket.uid)
+        socket.room = user.home_room
+        const room = db.prepare("SELECT * FROM rooms WHERE id = ?").get(user.home_room)
+        const room_model = db.prepare("SELECT * FROM room_models WHERE id = ?").get(room.model_name)//heightmap
+        const room_count = rooms.some(rm => rm.room.id == room.id)
+        if (!room_count) {
+            rooms.push({
+                room: room,
+                room_model: room_model,
+                players: []
+            })
+        }
+        const rm = rooms.find(rm => rm.room.id == room.id)
+        rm.players.push(user)
+    })
 
     socket.on("disconnect", () => {
+        if (socket.room) {
+            const room = rooms.find(rm => rm.room.id == socket.room)
+            room.players.forEach((ply, index) => {
+                if (ply.id == socket.uid) {
+                    room.players.splice(index, 1)
+                    if (!room.players.length) {
+                        rooms.forEach((rm, room_index) => {
+                            if (rm.room.id == socket.room) {
+                                rooms.splice(room_index, 1)
+                            }
+                        })
+                    }
+                }
+            })
+        }
     })
 })
 
 http.listen(8080, () => {
     console.log("Server Started")
 })
+
+setInterval(() => {
+    //nothing to do
+}, 1000 / 60)
